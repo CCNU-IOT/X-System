3fde26e0b1384211f61c5bcbe69789bfe4781147
sandeulllll <py820901@163.com>
2023-10-22 02:44:45 +0800

    web接入高德地图，显示区域为中国沿海地区

---
+++ xsystem02/src/main/java/com/ccnuiot/xsystem02/Xsystem02Application.java
@@ -0,0 +1,13 @@
+package com.ccnuiot.xsystem02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xsystem02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Xsystem02Application.class, args);
+    }
+
+}
---
+++ xsystem02/src/test/java/com/ccnuiot/xsystem02/Xsystem02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccnuiot.xsystem02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xsystem02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
